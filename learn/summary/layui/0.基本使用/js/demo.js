880d893539c6a879e3bcffe5b469881d05d2f539
water-li-drop <37493514+water-li-drop@users.noreply.github.com>
2019-07-01 18:11:53 +0800

    layui

---
+++ learn/summary/layui/0.基本使用/js/demo.js
@@ -0,0 +1,10 @@
+layui.define(function(exports) {
+
+    exports("demo", function() {
+        layui.data('test', {
+            key: 'nickname',
+            value: '贤心'
+        });
+        alert('demo');
+    });
+});
