170b680f7e7dade699cac4f2e811675cd7b7c5b2
water-li-drop <37493514+water-li-drop@users.noreply.github.com>
2019-07-04 20:09:05 +0800

    project

--- project/NovelReptiles/app.js
+++ project/NovelReptiles/app.js
@@ -4,29 +4,60 @@ const URL = require('url').URL;
 const gbk = require('gbk');
 const JSDOM = require("jsdom").JSDOM;
 
+var server = new http.Server();
+
+server.on('request', function(req, res) {
+    // res.writeHead(200, { 'Content-Type': 'text/html' });
+    // res.write('<h1>Node.js</h2>');
+    // res.end('<h1>Node.js</h2>');
+    // console.log(req);
+    if (req.url == '/') {
+        fs.readFile('C:\LYH\study\vscode_workspace\lyh-study\project\NovelReptiles\views\index.html', (err, data) => {
+            // console.log(err);
+            console.log('=======');
+            console.log(data);
+            res.write(data);
+            res.end();
+        });
+        // https://blog.csdn.net/k358971707/article/details/78775141
+    }
+
+});
+server.listen(3300);
+console.log('http://127.0.0.1:3300');
+
+
+
+
+
+
+
+
+
+
 // var url = 'https://www.qidian.com/all';
 var url = 'http://book.zongheng.com/store.html';
 // var url = 'https://item.taobao.com/item.htm?ft=t&id=575483010661&ali_trackid=2:mm_26632614_0_0:1562173804_131_1608789429&spm=a21bo.7925890.192091.3';
 // var url = 'http://pic37.nipic.com/20140113/8800276_184927469000_2.png';
 
-getData(url, function(data) {
-    // 将抓取的数据写入文件
-    // fs.appendFile('test.html', data, (err) => {
-    //     if (err) throw err;
-    //     console.log('文件已被保存');
-    // });
-    // 编码格式转换
-    // var html = gbk.toString(data);
-    // 获取页面dom
-    var dom = new JSDOM(data);
-    var documentTemp = dom.window.document;
-    var bookNameArr = documentTemp.querySelectorAll('.bookname a');
-    var a = bookNameArr.map(function(value) {
-        return value.innerHTML;
-    });
-    console.log(a);
-    // var buffer = Buffer.from(data);
-});
+// getData(url, function(data) {
+//     // 将抓取的数据写入文件
+//     // fs.appendFile('test.html', data, (err) => {
+//     //     if (err) throw err;
+//     //     console.log('文件已被保存');
+//     // });
+//     // 编码格式转换
+//     // var html = gbk.toString(data);
+//     // 获取页面dom
+//     var dom = new JSDOM(data);
+//     var documentTemp = dom.window.document;
+//     var bookNameArr = documentTemp.querySelectorAll('.bookname a');
+//     var a = bookNameArr.map(function(value) {
+//         return value.innerHTML;
+//     });
+//     console.log(a);
+//     // var buffer = Buffer.from(data);
+// });
 
 function getData(url, callback) {
     // url解析
--- project/NovelReptiles/public/js/index.js
+++ project/NovelReptiles/public/js/index.js
@@ -1,3 +1,13 @@
 layui.use('form', function() {
     var form = layui.form;
+    var $ = layui.$;
+    // $('').click(function{
+
+    // })
+    $.ajax({
+        url: ,
+
+    });
+
+
 });
