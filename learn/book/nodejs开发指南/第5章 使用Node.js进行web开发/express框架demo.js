7cf360744fa7367711627c6a3c0843dbb44c13af
water-li-drop <37493514+water-li-drop@users.noreply.github.com>
2019-07-08 19:20:46 +0800

    express

--- learn/book/nodejs开发指南/第5章 使用Node.js进行web开发/express框架demo.js
+++ learn/book/nodejs开发指南/第5章 使用Node.js进行web开发/express框架demo.js
@@ -12,20 +12,3 @@ var server = app.listen(3300, function() {
 
     console.log("应用实例，访问地址为 http://127.0.0.1", host, port)
 })
-
-//express_demo.js 文件
-// var express = require('express');
-// var app = express();
-
-// app.get('/', function (req, res) {
-//    res.send('Hello World');
-// })
-
-// var server = app.listen(8081, function () {
-
-//   var host = server.address().address
-//   var port = server.address().port
-
-//   console.log("应用实例，访问地址为 http://%s:%s", host, port)
-
-// })
